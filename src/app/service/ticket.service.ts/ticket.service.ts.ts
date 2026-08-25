d8d08fce88aa6963ca5d5ea0f09f515f55058250
AngelinaSatchi <angelinasashixinga@gmail.com>
2026-08-25 14:15:34 +0100

    exericios10

--- src/app/service/ticket.service.ts/ticket.service.ts.ts
+++ src/app/service/ticket.service.ts/ticket.service.ts.ts
@@ -1,8 +1,78 @@
 import { Injectable, computed, signal } from '@angular/core';
+import { Ticket, EstadoTicket, TECNICOS_MOCK } from '../../models/ticket.model';
 
-@Injectable({ providedIn: 'root'})
-  
+@Injectable({
+  providedIn: 'root'
+})
+export class TicketService {
 
-export class TicketServiceTs {}
+  private tickets = signal<Ticket[]>([
+  ]);
 
+  readonly ticketsReadonly = this.tickets.asReadonly();
 
+  readonly totalTickets = computed(() =>
+    this.tickets().length
+  );
+
+  readonly ticketsAbertos = computed(() =>
+    this.tickets().filter(t => t.estado === 'aberto').length
+  );
+
+  readonly ticketsEmProcesso = computed(() =>
+    this.tickets().filter(t => t.estado === 'em-progresso').length
+  );
+
+  readonly ticketsResolvidos = computed(() =>
+    this.tickets().filter(t => t.estado === 'resolvido').length
+  );
+
+  readonly ticketsFechados = computed(() =>
+    this.tickets().filter(t => t.estado === 'fechado').length
+  );
+
+  readonly ticketsCriticos = computed(() =>
+    this.tickets().filter(t => t.prioridade === 'critica').length
+  );
+
+  listar(): Ticket[] {
+    return this.tickets();
+  }
+
+  obterPorId(id: number): Ticket | undefined {
+    return this.tickets().find(ticket => ticket.id === id);
+  }
+
+  criar(ticket: Ticket): void {
+    this.tickets.update(tickets => [
+      ...tickets,
+      ticket
+    ]);
+  }
+
+  atualizarEstado(id: number, estado: EstadoTicket): void {
+    this.tickets.update(tickets =>
+      tickets.map(ticket =>
+        ticket.id === id
+          ? { ...ticket, estado }
+          : ticket
+      )
+    );
+  }
+
+  atribuirTecnico(id: number, nome: string): void {
+    this.tickets.update(tickets =>
+      tickets.map(ticket =>
+        ticket.id === id
+          ? { ...ticket, tecnico: nome }
+          : ticket
+      )
+    );
+  }
+
+  remover(id: number): void {
+    this.tickets.update(tickets =>
+      tickets.filter(ticket => ticket.id !== id)
+    );
+  }
+}
