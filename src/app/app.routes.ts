0952b8c1b1a87d67b39c1f9b9bec1ac1a1979dc7
AngelinaSatchi <angelinasashixinga@gmail.com>
2026-09-01 15:57:50 +0100

    Routas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,52 @@
 import { Routes } from '@angular/router';
 import { ListaTickets } from './pages/lista-tickets/lista-tickets';
 import { TicketCard } from './components/ticket-card/ticket-card';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { NovoTicket } from './pages/novo-ticket/novo-ticket';
+import { DetalheTicket } from './pages/detalhe-ticket/detalhe-ticket';
+import { NaoEncontrado } from './pages/nao-encontrado/nao-encontrado';
 
 export const routes: Routes = [
 
+  // {
+  //   path: 'tickets',
+  //   component: ListaTickets
+  // }
+   {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+
+
+
   {
     path: 'tickets',
-    component: ListaTickets
-  }
+    component: TicketCard,
+    title: 'Tickets'
+    
+  },
+
+  {
+    path: 'tickets/novo',
+    component: NovoTicket,
+    title: 'Novo Ticket'
+  },
+
+  {
+    path: 'tickets/:id',
+    component: DetalheTicket,
+    title: 'Detalhe do Ticket'
+  },
+
+  {
+    path: '**',
+    component: NaoEncontrado,
+    title: 'Página não encontrada'
+  },
+  {
+    path: '**',
+    component: Dashboard,
+  },
+
 ];
